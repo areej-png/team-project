465d859610cd7ac58ff4c7d94a6ea7e398787ce4
areej-png <areejamir890@gmail.com>
2025-10-07 08:06:43 -0700

    feat: add script.js for dynamic heading styling
    * feat: dynamically style heading using JavaScript
    
    * chore: minor test update after rebase

---
+++ script.js
@@ -0,0 +1,9 @@
+// Added minor styling to the heading
+document.addEventListener('DOMContentLoaded', function() {
+    const heading = document.querySelector('h1');
+    heading.style.color = 'grey';
+    heading.style.fontFamily = 'Arial, sans-serif';
+    heading.style.textAlign = 'center';
+    heading.style.marginTop = '50px';
+    console.log('Styles applied to heading');
+});
