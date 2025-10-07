ce610f88e5bb9664144cb670987d5ef725857e4b
areej-png <areejamir890@gmail.com>
2025-10-07 07:38:07 -0700

    feat: dynamically style heading using JavaScript

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const heading = document.querySelector('h1');
+    heading.style.color = 'grey';
+    heading.style.fontFamily = 'Arial, sans-serif';
+    heading.style.textAlign = 'center';
+    heading.style.marginTop = '50px';
+    console.log('Styles applied to heading');
+});
